369c47977f3c2587cb6eefafc128cc72d2987965
golden-rova <ogbekagolden@gmail.com>
2026-07-24 16:46:13 +0100

    cors update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,6 @@ export const corsList: string[] = CORS_ORIGINS
   : [
       "http://localhost:3000",
       "http://localhost:56266", // flutter web
+      "https://testimonies-com-web-admin.vercel.app/", // admin
+      "https://testimonies-com-web.vercel.app/", // user
     ]; // fallback if CORS_ORIGINS is not set
